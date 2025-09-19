57cb4476e257c2047a29e9b0fd7c20600c022b81
ArielFSoler <asole030@fiu.edu>
2025-09-19 15:07:53 -0700

    Week1 Coding Challenge Part 2 Complete

--- Week1/CodingChallenges/9_Classes/9_Classes/Human2.cs
+++ Week1/CodingChallenges/9_Classes/9_Classes/Human2.cs
@@ -6,6 +6,79 @@ namespace _9_ClassesChallenge
 {
     internal class Human2
     {
+        private string lastName = "Smyth";
+        private string firstName = "Pat";
+        //2.1
+        private string eyeColor;
+        private int? age = null;
+        //2.6
+        private int weight;
 
+        public Human2()
+        {
+
+        }
+
+        public Human2(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        //2.2
+        public Human2(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        public Human2(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public Human2(string firstName, string lastName, string eyeColor)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+        }
+
+        public string AboutMe()
+        {
+            return $"My name is {firstName} {lastName}.";
+        }
+
+        //2.3
+        public string AboutMe2()
+        {
+            if (string.IsNullOrWhiteSpace(eyeColor))
+                if (!age.HasValue)
+                    return $"My name is {firstName} {lastName}.";
+                else
+                    return $"My name is {firstName} {lastName}. My age is {age}";
+            else
+                if (!age.HasValue)
+                return $"My name is {firstName} {lastName}. My eye color is {eyeColor}.";
+            else
+                return $"My name is {firstName} {lastName}. My age is {age}. My eye color is {eyeColor}.";
+        }
+        
+        //2.6 Continued
+        public int Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value < 0 || value > 400)
+                    weight = 0;
+                else
+                    weight = value;
+            }
+        }
     }
 }
--- Week1/CodingChallenges/9_Classes/9_Classes/Program.cs
+++ Week1/CodingChallenges/9_Classes/9_Classes/Program.cs
@@ -14,6 +14,23 @@ namespace _9_ClassesChallenge
             //1.6
             Console.WriteLine(being1.AboutMe());
             Console.WriteLine(being2.AboutMe());
+
+            //2.4
+            Human2 being3 = new Human2("Jack", "Sparrow", "Brown");
+            Human2 being4 = new Human2("Hector", "Barbosa", 63);
+            Human2 being5 = new Human2("Elizabeth", "Swann", "Brown", 18);
+
+            //2.5
+            Console.WriteLine(being3.AboutMe2());
+            Console.WriteLine(being4.AboutMe2());
+            Console.WriteLine(being5.AboutMe2());
+
+            //2.7
+            Human2 being6 = new Human2("Davey", "Jones", "blue-green", 1045);
+            being6.Weight = 240;
+            Console.WriteLine($"When putting a valid weight(240), they weigh {being6.Weight}.");
+            being6.Weight = 666;
+            Console.WriteLine($"When putting an invalid weight(666), they weigh {being6.Weight}.");
         }
     }
 }
