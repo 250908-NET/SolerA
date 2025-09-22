ce094bad368ce192d7a4cc7258d17e48aa013d65
ArielFSoler <asole030@fiu.edu>
2025-09-22 07:51:09 -0700

    Week 2

--- Week1/ToDoApp/Program.cs
+++ Week1/ToDoApp/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using Serilog;
+
+namespace ToDoApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Configure logging before we "build" the app
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            builder.Host.UseSerilog();
+
+            var app = builder.Build();
+            app.MapGet("/", (ILogger<Program> logger) =>
+            {
+                return "Hello World!";
+            });
+
+            app.Run();
+        }
+    }
+}
